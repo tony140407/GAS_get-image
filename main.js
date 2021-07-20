4372a6a781bf7ae849dffff425b2b6071b2ffed8
tony140407 <a32715368@gmail.com>
2021-07-20 09:53:20 +0800

    exportImgParams & CDN版本號修改

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 eval(
   UrlFetchApp.fetch(
-    'https://cdn.jsdelivr.net/gh/tony140407/GAS_get-image@1.0/js/exportProperties.js'
+    'https://cdn.jsdelivr.net/gh/tony140407/GAS_get-image@1.1/js/exportProperties.js'
   ).getContentText()
 );
 eval(
@@ -31,22 +31,7 @@ const excelID = '';
 const beforeTime = 6;
 const during = 6;
 const imgProperties = exportProperties();
-const imgParams = [
-  '雷達-台灣周邊',
-  '雷達-全範圍',
-  '衛星雲圖-可見光_台灣',
-  '衛星雲圖-可見光_亞洲',
-  '衛星雲圖-色調強化_台灣',
-  '衛星雲圖-色調強化_亞洲',
-  '衛星雲圖-真實色_台灣',
-  '衛星雲圖-真實色_亞洲',
-  '氣溫',
-  '小時累積雨量',
-  '日累積雨量',
-  '探空',
-  'JMA天氣圖',
-  'NCDR風場',
-];
+const imgParams = exportImgParams();
 const dayArray = [20210719, 20210720]; // 你要創的日期 2021年天氣學 > 20210324 ...
 
 function runThisCodeToCreateFolders() {
@@ -73,20 +58,8 @@ getImg.setParameter(
 function part1() {
   getImg.getImgToFolder('雷達');
 }
-function part2_1() {
-  getImg.getImgToFolder('衛星雲圖', [
-    '衛星雲圖-可見光_台灣',
-    '衛星雲圖-可見光_亞洲',
-  ]);
-}
-function part2_2() {
-  getImg.getImgToFolder('衛星雲圖', [
-    '衛星雲圖-色調強化_台灣',
-    '衛星雲圖-色調強化_亞洲',
-  ]);
-}
-function part2_3() {
-  getImgToFolder('衛星雲圖', ['衛星雲圖-真實色_台灣', '衛星雲圖-真實色_亞洲']);
+function part2() {
+  getImg.getImgToFolder('衛星雲圖');
 }
 function part3() {
   getImg.getImgToFolder('氣溫');
@@ -98,3 +71,19 @@ function part4() {
   getImg.getImgToFolder('探空');
   getImg.getImgToFolder('JMA天氣圖');
 }
+
+// function part2_1() {
+//   getImg.getImgToFolder('衛星雲圖', [
+//     '衛星雲圖-可見光_台灣',
+//     '衛星雲圖-可見光_亞洲',
+//   ]);
+// }
+// function part2_2() {
+//   getImg.getImgToFolder('衛星雲圖', [
+//     '衛星雲圖-色調強化_台灣',
+//     '衛星雲圖-色調強化_亞洲',
+//   ]);
+// }
+// function part2_3() {
+//   getImgToFolder('衛星雲圖', ['衛星雲圖-真實色_台灣', '衛星雲圖-真實色_亞洲']);
+// }
